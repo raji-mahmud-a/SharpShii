6f2ed4543aa7eef9e30f14e3b339034aea9a868b
Raji Mahmud <raji@mahmud.dev>
2025-11-05 11:26:47 +0100

    hello world

--- controller.js
+++ controller.js
@@ -1,20 +1,7 @@
 import sharp from "sharp"
 
 const controller = (req, res)=>{
-  if(req.url === "/api"){
-   req.pipe(sharp()
-      .resize(300, 200) // The Sharp transform stream
-      .toFormat('webp')
-    )
-    .pipe(res);
-  }else{
-    res.writeHead(404, { "content-type": "application/json" })
-    res.end(JSON.stringify({
-      success: false,
-      message: 'Not Found',
-      details: "The resource you're looking for cannot be found: check the URL very well"
-    }, null, 2))
-  }
+  res.end("Hello World!🎭✨️")
 }
 
 
